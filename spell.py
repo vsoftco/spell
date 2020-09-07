b5595b139abf61e9bf8a7f5903e360bab0ae9e83
Vlad Gheorghiu <vsoftco@gmail.com>
2020-09-06 21:04:12 -0400

    Update spell.py

--- spell.py
+++ spell.py
@@ -9,7 +9,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2013 - 2020 Vlad Gheorghiu.
+# Copyright (c) 2018 - 2020 Vlad Gheorghiu.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
