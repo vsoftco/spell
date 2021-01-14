6c805e34d00dc506fbe7b7e01e5beab8a80a22dc
Vlad Gheorghiu <vsoftco@gmail.com>
2021-01-14 15:30:57 -0500

    lowercase

--- spell.py
+++ spell.py
@@ -71,13 +71,13 @@ if __name__ == "__main__":
             sys.exit(-1)
     else:
         # Use the NATO phonetic dictionary by default
-        dictionary = {'A': "Alpha", 'B': "Bravo", 'C': "Charlie", 'D': "Delta",
-                      'E': "Echo", 'F': "Foxtrot", 'G': "Golf", 'H': "Hotel",
-                      'I': "India", 'J': "Juliet", 'K': "Kilo", 'L': "Lima",
-                      'M': "Mike", 'N': "November", 'O': "Oscar", 'P': "Papa",
-                      'Q': "Quebec", 'R': "Romeo", 'S': "Sierra", 'T': "Tango",
-                      'U': "Uniform", 'V': "Victor", 'W': "Whiskey", 'X': "X-ray",
-                      'Y': "Yankee", 'Z': "Zulu", '1': "One", '2': "Two",
+        dictionary = {'a': "Alpha", 'b': "Bravo", 'c': "Charlie", 'd': "Delta",
+                      'e': "Echo", 'f': "Foxtrot", 'g': "Golf", 'h': "Hotel",
+                      'i': "India", 'j': "Juliet", 'k': "Kilo", 'l': "Lima",
+                      'm': "Mike", 'n': "November", 'o': "Oscar", 'p': "Papa",
+                      'q': "Quebec", 'r': "Romeo", 's': "Sierra", 't': "Tango",
+                      'u': "Uniform", 'v': "Victor", 'w': "Whiskey", 'x': "X-ray",
+                      'y': "Yankee", 'z': "Zulu", '1': "One", '2': "Two",
                       '3': "Three", '4': "Four", '5': "Five", '6': "Six",
                       '7': "Seven", '8': "Eight", '9': "Nine", '0': "Zero"}
 
